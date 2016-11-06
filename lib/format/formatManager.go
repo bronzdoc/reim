6a3843dc32c3ba63a26d7342f4edbb0ef77cbd42
bronzdoc <lsagastume1990@gmail.com>
2016-11-05 21:36:29 -0600

    Manage formaters with formatManager

---
+++ lib/format/formatManager.go
@@ -0,0 +1,47 @@
+package format
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+type FormatManager struct {
+	Formaters map[string]Formater
+	fileName  string
+}
+
+func NewFormatManager(fileName string) *FormatManager {
+	fm := FormatManager{
+		Formaters: make(map[string]Formater),
+		fileName:  fileName,
+	}
+
+	// Add formaters
+	fm.addFormat(NewJpeg())
+
+	return &fm
+}
+
+func (fm *FormatManager) Build() (Formater, error) {
+	bytearr, err := ioutil.ReadFile(fm.fileName)
+	if err != nil {
+		log.Println("Failed to read file: ", fm.fileName)
+		os.Exit(1)
+	}
+
+	for magicNumber := range fm.Formaters {
+		if strings.HasPrefix(string(bytearr), magicNumber) {
+			return fm.Formaters[magicNumber], nil
+		}
+	}
+
+	return nil, errors.New(fmt.Sprintf("Unknown format for: %v", fm.fileName))
+}
+
+func (fm *FormatManager) addFormat(formater Formater) {
+	fm.Formaters[formater.MagicNumber()] = formater
+}
